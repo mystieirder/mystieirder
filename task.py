d086abcaa241198b172fde2c034b7a6d8c530880
mystieirder <155094604+mystieirder@users.noreply.github.com>
2024-09-29 21:09:30 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,16 @@
+name = "pushkal sai a"
+age = 18
+clg = "Mysore Univerisity School of Engineering"
+choice = int(input("Enter the choice you want to view:"))
+while choice in range(0,4):
+    if choice == 0:
+        print(name)
+    elif choice == 1:
+        print(age)
+    elif choice == 2:
+        print(clg)
+    elif choice == 3:
+        print("#20,Srigurukrupa ,opp to sri sharada public school ,dattgalli 3rd stage, mysuru,570022")
+    else:
+        break
+    break
